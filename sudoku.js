b10f75222d885ec94cd498b9f777d18ccde17ad8
Alex <alekscherniaev@gmail.com>
2021-12-24 15:17:41 +0300

    add functions

--- sudoku.js
+++ sudoku.js
@@ -1,104 +1,52 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
  const fs = require ('fs')
 
- const boardString = fs.readFileSync('./sudoku-puzzles.txt','utf8').split('\n');
+// const boardString = fs.readFileSync('./sudoku-puzzles.txt','utf8').split('\n');
+// const {sudokuParse} = require('./runner')
 
- function solve(boardString) {
+
+//  function solve(boardString) {
   
-  let board1 = boardString.slice(0, 1).join().split('')
-  let board = []
-  for(let i = 0; i <board1.length; i += 9){
-   board.push(board1.slice(i, i+9))
+//   let board1 = boardString.slice(0, 1).join().split('')
+//   let board = []
+//   for(let i = 0; i <board1.length; i += 9){
+//    board.push(board1.slice(i, i+9))
+//   }
+
+//   return board
+// }
+// console.table(solve(boardString));
+
+
+
+function readFile(numberOfString) {
+  const text = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8').split('\n');
+  const string = text[numberOfString - 1];
+  console.log(string);
+  return string;
+}
+const string = readFile(1);
+function beautifyTheString(string) {
+  const beatyArr = [];
+  for (let i = 0; i < 81; i += 9) {
+    beatyArr.push(string.slice(i, i + 9).split(''));
   }
-
-  return board
+  return beatyArr;
 }
-console.table(solve(boardString));
+const board = beautifyTheString(string);
 
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
-
+function fillTheRow(board) {
+  while (board.flat().includes('-')) {
+    for (let i = 0; i < board.length; i += 1) {
+      for (let j = 0; j < board[i].length; j += 1) {
+        if (board[i][j] === '-') {
+          const random = `${Math.floor((Math.random() * 9) + 1)}`;
+          if (!board[i].includes(random)) {
+            board[i][j] = random;
+          }
+        }
+      }
+    }
+  }
+  return board;
 }
-
-
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {
-
-}
-
-// Exports all the functions to use them in another file.
-module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
-}
-// const fs = require('fs');
-// function readFile(numberOfString) {
-//   const text = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8').split('\n');
-//   const string = text[numberOfString - 1];
-//   console.log(string);
-//   return string;
-// }
-// const string = readFile(1);
-// function beautifyTheString(string) {
-//   const beatyArr = [];
-//   for (let i = 0; i < 81; i += 9) {
-//     beatyArr.push(string.slice(i, i + 9).split(''));
-//   }
-//   return beatyArr;
-// }
-// // console.log(beautifyTheString(string));
-// const board = beautifyTheString(string);
-// function fillTheRow(board) {
-//   while (board.flat().includes('-')) {
-//     for (let i = 0; i < board.length; i += 1) {
-//       for (let j = 0; j < board[i].length; j += 1) {
-//         if (board[i][j] === '-') {
-//           const random = `${Math.floor((Math.random() * 9) + 1`Q)}`;
-//           if (!board[i].includes(random)) {
-//             board[i][j] = random;
-//           }
-//         }
-//       }
-//     }
-//   }
-//   return board;
-// }
-// console.table(fillTheRow(board));
-// // Takes a board as a string in the format
-// // you see in the puzzle file. Returns
-// // something representing a board after
-// // your solver has tried to solve it.
-// // How you represent your board is up to you!
-// // function solve(boardString) {
-// // }
-// // Returns a boolean indicating whether
-// // or not the provided board is solved.
-// // The input board will be in whatever
-// // form `solve` returns.
-// // function isSolved(board) {
-// // }
-// // Takes in a board in some form and
-// // returns a String that's well formatted
-// // for output to the screen.
-// // The input board will be in whatever
-// // form `solve` returns.
-// // function prettyBoard(board) {
-// // }
-// // Exports all the functions to use them in another file.
-// // module.exports = {
-// // 	solve: solve,
-// // 	isSolved: isSolved,
-// // 	prettyBoard: prettyBoard
-// // }
+console.table(fillTheRow(board));
